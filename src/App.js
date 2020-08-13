3c5542e961f0c5b55781762e22de7845c1ccbf4b
bruno <brunoliracz@gmail.com>
2020-08-13 15:11:25 -0300

    buscando users na api e usando dropzone para upload de imagens

--- src/App.js
+++ src/App.js
@@ -1,24 +1,58 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState, useEffect, useCallback} from 'react';
 import './App.css';
+import api from './service/api';
+import {useDropzone} from 'react-dropzone'
+
+const Users = () =>{
+
+  const [user, setUser] = useState([]);
+  
+  const findUsers= () =>{
+    api.get('/item').then(res =>{
+      // console.log(res);
+      setUser(res.data);
+    });
+  };
+  useEffect(() => {
+    findUsers();
+  }, []);
+  return (
+    <div>
+      {user.map(user =>(
+        <div key={user.id}>
+          <br />
+        <h1>{user.name}</h1>
+        <Dropzone />
+        <br />
+        </div>
+      ))}
+    </div>
+  )
+
+};
+
+function Dropzone() {
+  const onDrop = useCallback(acceptedFiles => {
+    const file = acceptedFiles[0];
+  }, [])
+  const {getRootProps, getInputProps, isDragActive} = useDropzone({onDrop})
+
+  return (
+    <div {...getRootProps()}>
+      <input {...getInputProps()} />
+      {
+        isDragActive ?
+          <p>Drop the image here ...</p> :
+          <p>Drag and drop the image here, or click to select files</p>
+      }
+    </div>
+  )
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Users />
     </div>
   );
 }
